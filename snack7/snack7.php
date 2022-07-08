5e01da5750f480d3eb2d45687fbc4b46fe3b69f2
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 18:11:15 +0200

    printed names and surnames of pupils

--- snack7/snack7.php
+++ snack7/snack7.php
@@ -47,4 +47,49 @@ $classePrimaB = [
         ],
     ],
 ];
+
+var_dump($classePrimaB)
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Classe Prima B</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+        <h1 class="text-center text-primary">SEZIONE PRIMA B</h1>
+    </header>
+    <main>
+        <div class="container text-center py-5">
+            <?php for($i = 0; $i<count($classePrimaB); $i++ ){
+
+            ?>
+            <div>
+                <h2>
+                <!-- name -->
+                <?php echo  "<strong>Nome Alunno</strong> : " . $classePrimaB[$i]["name"]?>
+                </h2>
+
+                <h2>
+                    <!-- surname -->
+                    <?php echo "<strong>Cognome Alunno</strong>: " . $classePrimaB[$i]["surname"]?>
+                </h2>
+            </div>
+            
+
+                <h5>
+                    <!-- media voti -->
+                </h5>
+            <?php } ?>
+        </div>
+    </main>
+</body>
+</html>
