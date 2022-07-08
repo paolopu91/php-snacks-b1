99076dd5ecdfade8e9243d4103687f9825e1c52d
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 15:17:02 +0200

    added html and var_dump

--- snack1/snack1.php
+++ snack1/snack1.php
@@ -58,4 +58,53 @@ $basketGames = [
         "pointsVisitingTeam" => rand(40,150),
     ],
 ];
+
+// var_dump($basketGames);
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BaketGames</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</head>
+<body>
+    <header>
+    <!-- title snack -->
+    <div>
+        <h1 class="text-primary text-center">BASKET GAMES</h1>
+    </div>
+    </header>
+
+    <main>
+    <!-- result of array in php -->
+    <div class="container text-center">
+    <?php for($i; $i<count($basketGames); $i++){
+        ?>
+        <span>
+            <?php echo $basketGames[$i]["homeTeam"]; ?> 
+        </span>
+        <span> <?php echo $basketGames[$i]["PointsHomeTeam"]; ?> </span>
+        <br>
+        <span>
+            <?php echo $basketGames[$i]["visitingTeam"]; ?> 
+        </span>
+        <span> 
+            <?php echo $basketGames[$i]["PointsVisitingTeam"]; ?> 
+        </span>
+        
+    <?php } ?>
+    </div>
+    </main>
+
+</body>
+</html>
+
+
+
