466fcb7dfc4e940855461efb0f7af3ca61f9ea95
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 16:28:29 +0200

    added snack 2 first control on name

--- snack2/snack2.php
+++ snack2/snack2.php
@@ -4,3 +4,28 @@ Passare come parametri GET name, mail e age e verificare (cercando i metodi che 
 che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. 
 Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” 
 -->
+
+<?php
+
+$nameUser = key_exists("name", $_GET) ? trim($_GET["name"]) : false;
+$mailUser = key_exists("mail",$_GET) ? trim($_GET["mail"]) : false;
+$ageUser = key_exists("age",$_GET) ? trim($_GET["age"]): false;
+
+$userName = true;
+// condition for the nameUser
+if(!$nameUser){
+    echo "Nome Utente mancante";
+}else{
+
+    // here i will write all my condition for nameUser
+    if( strlen($nameUser)<= 3){
+        $userName=false;
+        echo "<span>Nome non valido</span>";
+
+    }elseif(strlen($nameUser) >= 3 ){
+        echo  "<strong>Nome Valido</strong>";
+    }
+    var_dump($nameUser);
+}
+
+?>
