bccfad3b4caab542118bfec42c314441421b5a8d
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 17:58:30 +0200

    added array classePrimaB

--- snack7/snack7.php
+++ snack7/snack7.php
@@ -3,3 +3,48 @@
 Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici.
  Stampare Nome, Cognome e la media dei voti di ogni alunno. 
 -->
+
+<?php
+$classePrimaB = [
+    [
+        "name"=>"Daniele",
+        "surname"=>"Lico",
+        "voti" =>[
+            "matematica" => 7,
+            "italiano" => 8,
+            "inglese" => 5,
+            "latino" => 6,
+        ],
+    ],
+    [
+        "name"=>"Flavio",
+        "surname"=>"Gonnellini",
+        "voti" =>[
+            "matematica" => 7,
+            "italiano" => 8,
+            "inglese" => 5,
+            "latino" => 6,
+        ],
+    ],
+    [
+        "name"=>"Marcello",
+        "surname"=>"Micarelli",
+        "voti" =>[
+            "matematica" => 7,
+            "italiano" => 8,
+            "inglese" => 5,
+            "latino" => 6,
+        ],
+    ],
+    [
+        "name"=>"Giuseppe Andrea",
+        "surname"=>"Cencioni",
+        "voti" =>[
+            "matematica" => 7,
+            "italiano" => 8,
+            "inglese" => 5,
+            "latino" => 6,
+        ],
+    ],
+];
+?>
