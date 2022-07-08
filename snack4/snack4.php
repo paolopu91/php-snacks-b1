a34b343f60b6f63da462fbaf787ba87f5c6302ee
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 17:53:23 +0200

    snack 3 ended

--- snack4/snack4.php
+++ snack4/snack4.php
@@ -2,3 +2,12 @@
 ## Snack 4
 Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
  -->
+ <?php
+
+
+//for cicle
+for($i = 0 ; $i < 15 ; $i++){
+$randomNumber[] = rand(1,100);
+}
+var_dump($randomNumber);
+ ?>
