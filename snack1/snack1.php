275e3ad0bebf5d0c7bffbf3b4a7cc434d54ef16c
paolopu91 <paolo.pugliese1991@gmail.com>
2022-07-08 14:29:17 +0200

    added array Basket Games in snack 1

--- snack1/snack1.php
+++ snack1/snack1.php
@@ -1,7 +1,61 @@
 <!-- 
-    
+
 ## Snack 1
-Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite,
+punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60
 
 -->
+
+<?php 
+$basketGames = [
+    [
+        "homeTeam" => "Atlanta Hawks",
+        "visitingTeam" => "Boston Celtics",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Chicago Bulls",
+        "visitingTeam" => "Charlotte Hornets",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Brooklyn Nets",
+        "visitingTeam" => "Cleveland Cavaliers",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Miami Heat",
+        "visitingTeam" => "New York Knicks",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Detroit Pistons",
+        "visitingTeam" => "Orlando Magic",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Philadelphia 76R",
+        "visitingTeam" => "Indiana Pacers",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Toronto Raptors",
+        "visitingTeam" => "Milwaukee Bucks",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+    [
+        "homeTeam" => "Dallas Mavericks",
+        "visitingTeam" => "Denver Nuggets",
+        "pointsHomeTeam" => rand(40, 150),
+        "pointsVisitingTeam" => rand(40,150),
+    ],
+];
+?>
